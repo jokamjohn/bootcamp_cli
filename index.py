10a6b39f74cfcdf1880a64fccd52063233955387
John Kagga <johnkagga@gmail.com>
2017-07-05 14:54:26 +0300

    Added an intro to the news application enabling a user to select their prefered news source from the provided list

--- index.py
+++ index.py
@@ -4,4 +4,29 @@ import click
 # The entry point of the app
 @click.command()
 def cli():
-    pass
+    news_dict = get_news_sources()
+
+    get_intro()
+
+    news_source = click.prompt("Please select a news source by entering its number or press enter for BBC", default=1)
+
+
+# Return the news sources
+def get_news_sources():
+    return {1: 'bbc-news', 2: 'al-jazeera-english', 3: 'bloomberg', 4: 'buzzfeed'}
+
+
+# Return the intro message to show the user
+def get_intro():
+    click.secho("~" * 130, fg='blue')
+    click.secho("Welcome to News-Breaker", fg='green')
+    click.secho("~" * 130, fg='blue')
+    click.echo("\n")
+
+    click.secho("NEWS SOURCES", fg='green')
+    click.secho('''        
+        1 - BBC
+        2 - AL Jazeera 
+        3 - Bloomberg
+        4 - BuzzFeed
+    ''', fg='blue')
