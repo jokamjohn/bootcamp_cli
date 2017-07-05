29f40afed719fd1dde9912a3d203fa37edc8f281
John Kagga <johnkagga@gmail.com>
2017-07-05 15:05:23 +0300

    Refactored the code and also added doc strings

--- index.py
+++ index.py
@@ -5,11 +5,14 @@ import requests
 # The entry point of the app
 @click.command()
 def cli():
-    news_dict = get_news_sources()
-
+    # Show the user the intro message
     get_intro()
 
+    # Prompt the user to enter a news source
     news_source = click.prompt("Please select a news source by entering its number or press enter for BBC", default=1)
+
+    # Dictionary containing the news sources
+    news_dict = get_news_sources()
 
     if news_source in news_dict.keys():
         get_news_articles(news_dict[news_source])
@@ -20,22 +23,6 @@ def cli():
 # Return the news sources
 def get_news_sources():
     return {1: 'bbc-news', 2: 'al-jazeera-english', 3: 'bloomberg', 4: 'buzzfeed'}
-
-
-# Return the intro message to show the user
-def get_intro():
-    click.secho("~" * 130, fg='blue')
-    click.secho("Welcome to News-Breaker", fg='green')
-    click.secho("~" * 130, fg='blue')
-    click.echo("\n")
-
-    click.secho("NEWS SOURCES", fg='green')
-    click.secho('''        
-        1 - BBC
-        2 - AL Jazeera 
-        3 - Bloomberg
-        4 - BuzzFeed
-    ''', fg='blue')
 
 
 # Return the intro message to show the user
